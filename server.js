3c51c6a9a2b215867b1bd16ca1e280487279ce4b
Kimmo Ihanus <kimmo@ihmissuti.fi>
2018-04-24 19:14:41 +0000

    added mixpanel and ga tracking

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 var path = require('path');
-// var public = __dirname + "/public/";
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 app.use(express.static(path.join(__dirname, '/public')));
 var sentiment = require('sentiment');
+// grab the Mixpanel factory
+var Mixpanel = require('mixpanel');
+ 
+// create an instance of the mixpanel client
+var mixpanel = Mixpanel.init(process.env.mixpanel);
 
 io.on('connection', function(socket){
+    
+    mixpanel.track('page load', {
+        distinct_id: socket.id
+    });
     
     socket.on('rating', function(rating){
         
@@ -17,22 +25,20 @@ io.on('connection', function(socket){
         console.dir(r1);
         if (r1.score == -5 || r1.score == -4) {
             var score = 1
-            // var comment = "Maybe this one is better:"
         } else if (r1.score == -3 || r1.score == -2) {
             var score = 2
-            // var comment =  "Maybe this one is better:"
         } else if (r1.score == -1 || r1.score == 0 || r1.score == 1) {
             var score = 3
-            // var comment = "We suggest this for you:"
         } else if (r1.score == 2 || r1.score == 3) {
             var score = 4
-            // var comment = "Then you might like this also:"
         } else {
             var score = 5
-            // var comment = "Then I'm sure you'll like this one also:"
         }
         socket.emit('score', score)
-        // socket.emit('comment', comment)
+        
+        mixpanel.track('review', {
+            review: rating
+        });
 
     })
     
